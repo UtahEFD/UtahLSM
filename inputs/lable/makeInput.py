101a098556805b339e7796c0f938902017707dbb
Jeremy Gibbs <jeremy.gibbs@utah.edu>
2017-10-10 16:58:06 -0600

    Corrected input data to reflect starting at 00UTC instead of 12

--- inputs/lable/makeInput.py
+++ inputs/lable/makeInput.py
@@ -9,16 +9,13 @@ import numpy as np
 
 # soil sensor depths and interpolation levels
 measZ = [0.0, 0.05, 0.15, 0.25, 0.35, 0.60]
-#initZ = [0.0, 0.05, 0.025, 0.05, 0.075, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 1.0, 2.0]
 nSoil = len(measZ)
 
 # soil temperature
-measSoilTemp = [295.05, 293.09, 293.28, 293.22, 293.15, 292.63]
-#initSoilTemp = np.interp(initZ, measZ, measSoilTemp)
+measSoilTemp = [297.18, 295.52, 294.75, 293.69, 293.35, 292.66]
 
 # soil moisture
-measSoilMois = [0.23, 0.25, 0.26, 0.27, 0.33, 0.34]
-#initSoilMois = np.interp(initZ, measZ, measSoilMois)
+measSoilMois = [0.1632, 0.2531, 0.2532, 0.2744, 0.3283, 0.32082]
 
 # soil properties
 # Lamont - 0-19: silty loam, 20-34: clay, 35-60: clay loam
@@ -29,21 +26,6 @@ soilExponent = np.zeros((nSoil,1))
 heatCapSoil  = np.zeros((nSoil,1))
 profileTemp  = np.zeros((nSoil,1))
 profileMois  = np.zeros((nSoil,1))
-
-# if interpolating to more depths
-#porosity[:,0]     =  [0.485,0.485, 0.485,0.485,0.485,0.485,0.482,0.482,0.482,0.476,0.476,0.476,0.476,0.476,0.476,0.476,0.476]
-#satPotential[:,0] =  [0.786,0.786,0.786,0.786,0.786,0.786,0.405,0.405,0.405,0.630,0.630,0.630,0.630,0.630,0.630,0.630,0.630]
-#satHydrCond[:,0]  =  [7.2,7.2,7.2,7.2,7.2,7.2,1.3,1.3,1.3,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5]
-#soilExponent[:,0] =  [5.3,5.3,5.3,5.3,5.3,5.3,11.4,11.4,11.4,8.52,8.52,8.52,8.52,8.52,8.52,8.52,8.52]
-#heatCapSoil[:,0]  =  [1.27,1.27,1.27,1.27,1.27,1.27,1.09,1.09,1.09,1.23,1.23,1.23,1.23,1.23,1.23,1.23,1.23]
-#porosity[:,0]     =  [0.485,0.485, 0.485,0.485,0.485,0.485,0.482,0.482,0.482,0.476,0.476,0.476,0.476,0.476,0.476,0.476,0.476]
-#satPotential[:,0] =  [0.786,0.786,0.786,0.786,0.786,0.786,0.405,0.405,0.405,0.630,0.630,0.630,0.630,0.630,0.630,0.630,0.630]
-#satHydrCond[:,0]  =  [7.2,7.2,7.2,7.2,7.2,7.2,1.3,1.3,1.3,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5]
-#soilExponent[:,0] =  [5.3,5.3,5.3,5.3,5.3,5.3,11.4,11.4,11.4,8.52,8.52,8.52,8.52,8.52,8.52,8.52,8.52]
-#heatCapSoil[:,0]  =  [1.27,1.27,1.27,1.27,1.27,1.27,1.09,1.09,1.09,1.23,1.23,1.23,1.23,1.23,1.23,1.23,1.23]
-
-#profileTemp[:,0]  = initSoilTemp
-#profileMois[:,0]  = initSoilMois
 
 porosity[:,0]     =  [0.485,0.485,0.485,0.482,0.476,0.476]
 satPotential[:,0] =  [0.786,0.786,0.786,0.405,0.630,0.630]
@@ -64,7 +46,6 @@ heatCapSoil  = heatCapSoil * 1e6
 soilOut = np.r_[porosity.T, satPotential.T, satHydrCond.T, soilExponent.T, heatCapSoil.T]
 
 # write input files
-#np.savetxt('soilLevels.ini',       initZ,       fmt='%.7e',  delimiter='\t', newline='\n')
 np.savetxt('soilLevels.ini',       measZ,       fmt='%.7e',  delimiter='\t', newline='\n')
 np.savetxt('soilTypeParams.ini',   soilOut,     fmt='%14.7e',delimiter='\t', newline='\n')
 np.savetxt('soilTemperature.ini',  profileTemp, fmt='%.7e',  delimiter='\t', newline='\n')
@@ -77,17 +58,13 @@ np.savetxt('soilMoisture.ini',     profileMois, fmt='%.7e',  delimiter='\t', new
 # open MET data
 met = nc.MFDataset('sgpmetE13.b1.*.cdf')
 
-# time indices (12UTC-12UTC)
-tstart = 0
-tfinal = 2880
-
 # grab variables
-tt = met.variables['time'][tstart:tfinal+1]
-ws = met.variables['wspd_vec_mean'][tstart:tfinal+1]
-wd = met.variables['wdir_vec_mean'][tstart:tfinal+1]
-pt = met.variables['temp_mean'][tstart:tfinal+1] + 273.15
-pa = met.variables['atmos_pressure'][tstart:tfinal+1]
-pv = met.variables['vapor_pressure_mean'][tstart:tfinal+1]
+tt = met.variables['time'][:]
+ws = met.variables['wspd_vec_mean'][:]
+wd = met.variables['wdir_vec_mean'][:]
+pt = met.variables['temp_mean'][:] + 273.15
+pa = met.variables['atmos_pressure'][:]
+pv = met.variables['vapor_pressure_mean'][:]
 
 # fix times to be continuous across days
 tt[1440::] = tt[1440::]+tt[1439]+60
@@ -99,26 +76,9 @@ vc = -ws*np.cos(wd*np.pi/180)
 # compute water vapor mixing ratio
 qv = 0.622 * pv / (pa-pv)
 
-# interpolate between times to increase input times
-#dt     = 10
-#ttNew  = np.arange(tt[0],tt[-1]+1,dt) 
-#nTimes = len(ttNew)
-#
-#ucNew = np.interp(ttNew, tt, uc)
-#vcNew = np.interp(ttNew, tt, vc)
-#qvNew = np.interp(ttNew, tt, qv)
-#ptNew = np.interp(ttNew, tt, pt)
-#
-ttNew = tt
-nTimes = len(tt)
-ucNew = uc
-vcNew = vc
-qvNew = qv
-ptNew = pt
-
 of = open('timeSeriesMET.dat','w')
-for t in range(len(ttNew)):
-	os = "{0:.6E}\t{1:13.6E}\t{2:2.6E}\t{3:2.6E}\t{4:2.6E}\t{5:2.6E}\t{6:2.6E}\n".format(ttNew[t], ucNew[t], vcNew[t], 0, ptNew[t], qvNew[t],0)
+for t in range(len(tt)):
+	os = "{0:.6E}\t{1:13.6E}\t{2:2.6E}\t{3:2.6E}\t{4:2.6E}\t{5:2.6E}\t{6:2.6E}\n".format(tt[t], uc[t], vc[t], 0, pt[t], qv[t],0)
 	of.write(os)
 of.close()
 
@@ -129,20 +89,16 @@ of.close()
 # open CO2FLX data
 data = nc.MFDataset('sgp30co2flx4mmetC1.b1.*')
 
-# time indices
-tstart = 0
-tfinal = 96
-
 # grab variables
-tt   = data.variables['time_offset'][tstart:tfinal+1]
-lwD  = data.variables['r_down_long_hemisp'][tstart:tfinal+1]
-lwU  = data.variables['r_up_long_hemisp'][tstart:tfinal+1]
-swD  = data.variables['r_down_short_hemisp'][tstart:tfinal+1]
-swU  = data.variables['r_up_short_hemisp'][tstart:tfinal+1]
-rNet = data.variables['r_net'][tstart:tfinal+1]
-G    = data.variables['mean_g_soil'][tstart:tfinal+1]
-H    = data.variables['h'][tstart:tfinal+1]
-LE   = data.variables['le'][tstart:tfinal+1]
+tt   = data.variables['time_offset'][:]
+lwD  = data.variables['r_down_long_hemisp'][:]
+lwU  = data.variables['r_up_long_hemisp'][:]
+swD  = data.variables['r_down_short_hemisp'][:]
+swU  = data.variables['r_up_short_hemisp'][:]
+rNet = data.variables['r_net'][:]
+G    = data.variables['mean_g_soil'][:]
+H    = data.variables['h'][:]
+LE   = data.variables['le'][:]
 
 # fix times to be continuous across days
 tt[48::] = tt[48::]+tt[47]+1800
