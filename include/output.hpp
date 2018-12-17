75ad6535b779aaa4d66c5390e7bad0529629e76d
Jeremy Gibbs <jeremy.gibbs@utah.edu>
2018-12-16 23:39:46 -0700

    Added generic output class. Data is not updating properly, but mechanics work.

---
+++ include/output.hpp
@@ -0,0 +1,45 @@
+//
+//  input.hpp
+//  
+//  This class handles reading in user options
+//
+//  Created by Jeremy Gibbs on 6/29/17.
+//
+
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include <netcdf>
+
+/**
+ * This class handles saving output files.
+ */
+
+using namespace netCDF;
+using namespace netCDF::exceptions;
+
+class Output {
+    
+    private:
+    
+        // netCDF variables
+        NcFile* outFile;
+        std::map<std::string,NcVar> fields;
+    
+    public:
+    
+        // initializer
+        Output();
+    
+        // setter
+        NcDim addDimension(std::string, int size=0);
+        void addField(std::string, std::string, std::string, std::vector<NcDim>);
+        void saveField1D(std::string name, const std::vector<size_t>, double);
+        void saveField2D(std::string name, const std::vector<size_t>,
+                         std::vector<size_t>, std::vector<double>);
+};
+
+#endif
---
+++ src/output.cpp
@@ -0,0 +1,63 @@
+//
+//  input.cpp
+//  
+//  This class handles reading in user options
+//  This is modified from version in MicroHH
+//
+//  Created by Jeremy Gibbs on 6/29/17.
+//
+
+#include "output.hpp"
+#include "utah_lsm.hpp"
+#include "constants.hpp"
+#include <iostream>
+
+using namespace netCDF;
+using namespace netCDF::exceptions;
+
+namespace {
+    namespace c = Constants;
+}
+
+Output :: Output() {
+    
+    // create output file
+    outFile = new NcFile("lsm.nc", NcFile::replace);
+}
+
+NcDim Output :: addDimension(std::string name, int size) {
+    
+    if (size) {
+        return outFile->addDim(name, size);
+    } else {
+        return outFile->addDim(name);
+    }
+}
+
+void Output :: addField(std::string name, std::string units,
+                        std::string long_name, std::vector<NcDim> dims) {
+ 
+    // add field
+    NcVar var = outFile->addVar(name, ncDouble, dims);
+    var.putAtt("units", units);
+    var.putAtt("long_name", long_name);
+    
+    // map name to field for later access
+    fields[name] = var;
+}
+
+void Output :: saveField1D(std::string name, const std::vector<size_t> index,
+                           double data) {
+    
+    // write output data
+    NcVar var = fields[name];
+    var.putVar(index, data);
+}
+
+void Output :: saveField2D(std::string name, const std::vector<size_t> index,
+                           std::vector<size_t> size, std::vector<double> data) {
+    
+    // write output data
+    NcVar var = fields[name];
+    var.putVar(index, size, &data[0]);
+}
