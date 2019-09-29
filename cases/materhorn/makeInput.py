461df5f6c1ad375a229c041d1c00c2a7719843a3
Jeremy Gibbs <jeremy.gibbs@ou.edu>
2019-09-29 15:53:28 -0500

    Added new materhorn data

--- cases/materhorn/makeInput.py
+++ cases/materhorn/makeInput.py
@@ -3,178 +3,143 @@ import sys, json
 import netCDF4 as nc
 import numpy as np
 import pylab as pl
+import datetime as dt
 
-##########################################
-# Soil data taken from Cesar Observatory #
-##########################################
+#############
+# Constants #
+#############
+Lv = 2.5E6
+Rd = 287
+Rv = 461
+T0 = 273.15
+E0 = 6.11
 
-# measurements are saved 10 minutes
-# so there are 144 per day
-date = 2
-tidx = (date-1)*144
+######################################
+# Data from Materhorn Field Campaign #
+######################################
 
-# soil temperature data
-obs   = open('observations/efs_sage_ebsoil_3_05_2013_5min.dat','r').read().split()[40::]
+# materhorn data files
+obs = open('observations/efs_sage_iop5_5min.dat','r').read().split()[20::]
+lem = open('observations/lems_iop5_10sec.dat','r').read().split()[17::]
 
-YR = obs[0::40]
-MO = obs[1::40]
-DA = obs[2::40]
-HH = obs[3::40]
-MM = obs[4::40]
+# atmospheric data
+pa    = np.array(obs[5::20]).astype(float)*100.0
+ws    = np.array(obs[6::20]).astype(float)
+wd    = np.array(obs[7::20]).astype(float)
+pt    = np.array(obs[8::20]).astype(float)+T0
+rh    = np.array(obs[9::20]).astype(float)
+net   = np.array(obs[10::20]).astype(float)
+ntime = len(ws)
 
-netr = np.array([float(x) for x in obs[9::40]])
+# convert relative humidity to mixing ratio
+es = E0*np.exp(Lv/Rv * (1/T0 - 1/pt))
+e  = rh*es/100
+sh = e * Rd / (Rv * (pa/100 - e))
+qs = sh / (sh + 1)
 
-pl.plot(netr)
-pl.show()
-sys.exit()
+# soil moisture
+sm_02 = np.float(lem[15])
+sm_07 = np.float(obs[11])
+sm_25 = np.float(lem[13])
+sm_00 = sm_02
+sm_ob = np.array([sm_00,sm_02,sm_07,sm_25])
+z_obm = np.array([0.000,0.025,0.075,0.25])
 
-t_obs = obs.variables['time'][tidx]*3600.
-st_00 = obs.variables['TS00'][tidx]+273.15
-st_02 = obs.variables['TS02'][tidx]+273.15
-st_04 = obs.variables['TS04'][tidx]+273.15
-st_06 = obs.variables['TS06'][tidx]+273.15
-st_08 = obs.variables['TS08'][tidx]+273.15
-st_12 = obs.variables['TS12'][tidx]+273.15
-st_20 = obs.variables['TS20'][tidx]+273.15
-st_30 = obs.variables['TS30'][tidx]+273.15
-st_50 = obs.variables['TS50'][tidx]+273.15
-st_ob = np.array([st_00,st_02,st_04,st_04,st_08,st_12,st_20,st_30,st_50])
-z_obs = np.array([0.00,0.02,0.04,0.06,0.08,0.12,0.20,0.30,0.50])
+# soil temperature
+st_01 = np.float(obs[12])+T0
+st_02 = np.float(obs[13])+T0
+st_05 = np.float(obs[14])+T0
+st_07 = np.float(obs[15])+T0
+st_10 = np.float(obs[16])+T0
+st_15 = np.float(obs[17])+T0
+st_25 = np.float(obs[18])+T0
+st_70 = np.float(obs[19])+T0
+st_00 = st_01
+st_ob = np.array([st_00,st_01,st_02,st_05,st_07,st_10,st_15,st_25,st_70])
+z_obs = np.array([0.000,0.01,0.025,0.050,0.075,0.100,0.150,0.250,0.700])
 nsoil = len(z_obs)
 
-# soil moisture data (10-minute)
-#obs   = nc.Dataset('observations/cesar_soil_water_lb1_t10_v1.1_200607.nc')
-#sm_03 = obs.variables['TH03'][tidx]
-#sm_08 = obs.variables['TH08'][tidx]
-#sm_20 = obs.variables['TH20'][tidx]
-#
-## soil moisture data (daily average from prior day)
-## Julian day for July 2, 2006 is 183, so day prior is 182
-#jdi   = 181
-#obs   = nc.Dataset('observations/cesar_tdr_soilmoisture_la1_t1d_v1.0_2006.nc')
-#sm_05 = np.mean([obs.variables['SM1'][jdi],obs.variables['SM7'][jdi], obs.variables['SM13'][jdi],obs.variables['SM19'][jdi]])
-#sm_15 = np.mean([obs.variables['SM2'][jdi],obs.variables['SM8'][jdi], obs.variables['SM14'][jdi],obs.variables['SM20'][jdi]])
-#sm_30 = np.mean([obs.variables['SM3'][jdi],obs.variables['SM9'][jdi], obs.variables['SM15'][jdi],obs.variables['SM21'][jdi]])
-#sm_45 = np.mean([obs.variables['SM4'][jdi],obs.variables['SM10'][jdi],obs.variables['SM16'][jdi],obs.variables['SM22'][jdi]])
-#sm_60 = np.mean([obs.variables['SM5'][jdi],obs.variables['SM11'][jdi],obs.variables['SM17'][jdi],obs.variables['SM23'][jdi]])
-#sm_73 = np.mean([obs.variables['SM6'][jdi],obs.variables['SM12'][jdi],obs.variables['SM18'][jdi],obs.variables['SM24'][jdi]])				
-#
-#sm_ob = np.array([sm_05,sm_05,sm_15,sm_30,sm_45,sm_60,sm_73])
-#z_obm = np.array([0.0,0.05,0.15,0.30,0.45,0.60,0.725])
-#
-## interpolate soil moisture to temperature grid
-#sm_oi = np.interp(z_obs,z_obm,sm_ob)
-#
-## soil type from USDA 11-category + peat
-##  1 = sand
-##  2 = loamy sand
-##  3 = sandy loam
-##  4 = silty loam
-##  5 = loam
-##  6 = sandy clay loam
-##  7 = silty clay loam
-##  8 = clay loam
-##  9 = sandy clay
-## 10 = silty clay
-## 11 = clay
-## 12 = peat
-#stype = np.full((nsoil),11)
-#
-####################################
-## Read met tower data for u,v,T,q #
-####################################
-#
-## end time is 9 hours, so 6*9 = 54
-#tend  = tidx+55
-#
-## open MET data
-#met = nc.MFDataset('observations/cesar_surface_meteo_lc1_t10_v1.0_200607.nc')
-#
-## grab variables
-#tm = met.variables['time'][tidx:tend]*3600.
-#ws = met.variables['F010'][tidx:tend]
-#wd = met.variables['D010'][tidx:tend]
-#pt = met.variables['TA002'][tidx:tend]
-#pa = met.variables['P0'][tidx:tend]
-#qs = met.variables['Q002'][tidx:tend]/1000
-#
-## compute wind components
-#uc = -ws*np.sin(wd*np.pi/180)
-#vc = -ws*np.cos(wd*np.pi/180)
-#
-## time dimension
-#dt    = tm[1] - tm[0]
-#ntime = len(tm)
-#t_utc = (tm-86400)%86400
-#
-##################################
-## Read radiation data for R_net #
-##################################
-#rad = nc.MFDataset('observations/cesar_surface_radiation_lc1_t10_v1.0_200607.nc')
-#swu = rad.variables['SWU'][tidx:tend]
-#swd = rad.variables['SWD'][tidx:tend]
-#lwu = rad.variables['LWU'][tidx:tend]
-#lwd = rad.variables['LWD'][tidx:tend]
-#net = swd-swu+lwd-lwu
-#
-###############################
-## Write all time series data #
-###############################
-#metr = {}
-#metr['time'] = {}
-#metr['data'] = {}
-#
-#metr['time']['ntime'] = ntime 
-#metr['time']['tstep'] = float(dt)
-#metr['data']['atm_U'] = ws.tolist()
-#metr['data']['atm_T'] = pt.tolist()
-#metr['data']['atm_q'] = qs.tolist()
-#metr['data']['atm_p'] = pa.tolist()
-#metr['data']['R_net'] = net.tolist()
-#with open('inputOffline.json', 'w') as outfile:  
-#    json.dump(metr,outfile,indent=4)
-#
-#########################
-## Settings for UtahLSM #
-#########################
-#namelist = {}
-#namelist['grid'] = {}
-#namelist['length'] = {}
-#namelist['soil'] = {}
-#namelist['radiation'] = {}
-#namelist['output'] = {}
-#
-## grid section
-#namelist['grid']['nx'] = 1
-#namelist['grid']['ny'] = 1
-#
-## length scale section
-#namelist['length']['z_o'] = 0.15
-#namelist['length']['z_t'] = 0.0015
-#namelist['length']['z_m'] = 10.0 
-#namelist['length']['z_s'] = 2.0
-#
-## soil section
-#namelist['soil']['nsoil']     = nsoil
-#namelist['soil']['param']     = 3
-#namelist['soil']['model']     = 2
-#namelist['soil']['soil_z']    = z_obs.tolist()
-#namelist['soil']['soil_type'] = stype.tolist()
-#namelist['soil']['soil_T']    = st_ob.tolist()
-#namelist['soil']['soil_q']    = sm_oi.tolist()
-#
-## radiation section
-#namelist['radiation']['utc_start']  = t_utc[0] 
-#namelist['radiation']['comp_rad']   = 0
-#namelist['radiation']['albedo']     = 0.33
-#namelist['radiation']['emissivity'] = 0.99
-#namelist['radiation']['latitude']   = 51.9711
-#namelist['radiation']['longitude']  = -4.9267
-#namelist['radiation']['julian_day'] = 183
-#
-## output section
-#namelist['output']['save'] = 1
-#namelist['output']['fields'] = ['all']
-#
-#with open('inputLSM.json', 'w') as outfile:  
-#    json.dump(namelist,outfile,indent=4)
+# interpolate soil moisture to temperature grid
+sm_oi = np.interp(z_obs,z_obm,sm_ob)
+
+# soil type from USDA 11-category + peat
+#  1 = sand
+#  2 = loamy sand
+#  3 = sandy loam
+#  4 = silty loam
+#  5 = loam
+#  6 = sandy clay loam
+#  7 = silty clay loam
+#  8 = clay loam
+#  9 = sandy clay
+# 10 = silty clay
+# 11 = clay
+# 12 = peat
+stype = np.full((nsoil),4)
+
+##############################
+# Write all time series data #
+##############################
+metr = {}
+metr['time'] = {}
+metr['data'] = {}
+
+metr['time']['ntime'] = ntime 
+metr['time']['tstep'] = 300.0
+metr['data']['atm_U'] = ws.tolist()
+metr['data']['atm_T'] = pt.tolist()
+metr['data']['atm_q'] = qs.tolist()
+metr['data']['atm_p'] = pa.tolist()
+metr['data']['R_net'] = net.tolist()
+with open('inputOffline.json', 'w') as outfile:  
+    json.dump(metr,outfile,indent=4)
+
+########################
+# Settings for UtahLSM #
+########################
+namelist = {}
+namelist['time'] = {}
+namelist['grid'] = {}
+namelist['length'] = {}
+namelist['soil'] = {}
+namelist['radiation'] = {}
+namelist['output'] = {}
+
+# grid section
+namelist['time']['step_seb'] = 10
+namelist['time']['step_dif'] = 10
+
+# grid section
+namelist['grid']['nx'] = 1
+namelist['grid']['ny'] = 1
+
+# length scale section
+namelist['length']['z_o'] = 0.15
+namelist['length']['z_t'] = 0.0015
+namelist['length']['z_m'] = 10.0 
+namelist['length']['z_s'] = 2.0
+
+# soil section
+namelist['soil']['nsoil']     = nsoil
+namelist['soil']['param']     = 3
+namelist['soil']['model']     = 2
+namelist['soil']['soil_z']    = z_obs.tolist()
+namelist['soil']['soil_type'] = stype.tolist()
+namelist['soil']['soil_T']    = st_ob.tolist()
+namelist['soil']['soil_q']    = sm_oi.tolist()
+
+# radiation section
+namelist['radiation']['utc_start']  = 12.0 
+namelist['radiation']['comp_rad']   = 0
+namelist['radiation']['albedo']     = 0.33
+namelist['radiation']['emissivity'] = 0.99
+namelist['radiation']['latitude']   = 40.121360
+namelist['radiation']['longitude']  = -113.129070
+namelist['radiation']['julian_day'] = 133
+
+# output section
+namelist['output']['save'] = 1
+namelist['output']['fields'] = ['all']
+
+with open('inputLSM.json', 'w') as outfile:  
+    json.dump(namelist,outfile,indent=4)
