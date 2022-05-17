37d5116c47d657ec9794cd4ce3a8492fd9443d9f
Jeremy Gibbs <jeremy.gibbs@noaa.gov>
2022-05-17 22:10:20 +0000

    Added constants to py from cpp

--- python/util/constants.py
+++ python/util/constants.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
-grav = 9.81 # [m/s^2] gravity
-vonk = 0.4  # []      von Karman constant
+vonk    = 0.4              # von Karman constant []
+grav    = 9.81             # acceleration due to gravity [m/s^2]
+pi      = 3.14159265358979 # pi
+sb      = 5.6697e-8        # Stefan-Boltzmann constant [W/m^2-K^4]
+sc      = 1367             # solar constant [K-m/s]
+rho_air = 1.204            # density of air [kg/m^3]
+rho_wat = 1000.0           # density of water [kg/m^3]
+Rv      = 461.4            # gas constant for water vapor [J/kg-K]
+Lv      = 2.45e6           # latent heat of vaporization [J/kg]
+Ci_wat  = 4186000.0        # volumetric heat capacity of water [J/m^3-K]
+Cp_air  = 1004.0           # specific heat of air [J/kg-K]
