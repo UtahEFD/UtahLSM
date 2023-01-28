cb9c20b0fa7be533e277eb8999a4d3b61c00282f
Jeremy Gibbs <jeremy.gibbs@noaa.gov>
2023-01-27 22:06:28 -0600

    Cleaned up the Settings class

--- cpp/include/settings.hpp
+++ cpp/include/settings.hpp
@@ -24,33 +24,33 @@ using json = nlohmann::json;
 /**
  * Class for managing json settings / data files.
  * 
- * This class is responsible for opening a supplied input file
+ * This class is responsible for opening a supplied settings file
  * and returning requested fields from that file.
  */
 class Settings {
     
     private:
 
-        json input;      ///< json wrapper of input file
+        json settings; ///< json wrapper of settings file
         
         /**
-         * Reads a json input file.
+         * Reads a json settings file.
          *
-         * @param[in] input_file name of input file
+         * @param[in] settings_file name of settings file
          */
-        void readInputFile(std::string input);
+        void readSettingsFile(std::string settings_file);
     
     public:
 
         /**
-         * Constructs an Settings object.
+         * Constructs a Settings object.
          *
-         * @param[in] input_file name of input file
+         * @param[in] settings_file name of settings file
          */
-        Input(std::string input_file);        
+        Settings(std::string settings_file);        
         
         /**
-         * Retrieves the requested integer from the input file and 
+         * Retrieves the requested integer from the settings file and 
          * places it in the supplied pointer.
          *
          * @param[out] external external integer pointer to fill with requested data
@@ -60,7 +60,7 @@ class Settings {
         void getItem(int& external, std::string section, std::string name);
         
         /**
-         * Retrieves the requested double from the input file and 
+         * Retrieves the requested double from the settings file and 
          * places it in the supplied pointer.
          *
          * @param[out] external external double pointer to fill with requested data
@@ -70,7 +70,7 @@ class Settings {
         void getItem(double& external, std::string section, std::string name);
         
         /**
-         * Retrieves the requested vector<int> from the input file and 
+         * Retrieves the requested vector<int> from the settings file and 
          * places it in the supplied pointer.
          *
          * @param[out] external external vector<int> pointer to fill with requested data
@@ -80,7 +80,7 @@ class Settings {
         void getItem(std::vector<int>& external, std::string section, std::string name);
         
         /**
-         * Retrieves the requested vector<double> from the input file and 
+         * Retrieves the requested vector<double> from the settings file and 
          * places it in the supplied pointer.
          *
          * @param[out] external external vector<double> pointer to fill with requested data
@@ -90,7 +90,7 @@ class Settings {
         void getItem(std::vector<double>& external, std::string section, std::string name);
         
         /**
-         * Retrieves the requested vector<string> from the input file and 
+         * Retrieves the requested vector<string> from the settings file and 
          * places it in the supplied pointer.
          *
          * @param[out] external external vector<string> pointer to fill with requested data
@@ -107,42 +107,42 @@ typedef void * SettingsObject; ///< Pointer representing Settings object
  */
 extern "C" {
     /**
-     * C-style wrapper for the Input constructor.
+     * C-style wrapper for the Settings constructor.
      *
-     * @param[in] input_file name of input file
+     * @param[in] settings_file name of settings file
      */
-    SettingsObject GetInput(char* input_file);
+    SettingsObject GetSettings(char* settings_file);
 
     /**
      * C-style wrapper for the getItem function for an integer.
      *
-     * @param[in]  input Settings object
+     * @param[in]  settings Settings object
      * @param[out] external external integer pointer to fill with requested data
      * @param[in]  section name of the section where the requested item resides in the json file
      * @param[in]  name name of the requested field in the json file
      */
-    void GetItemInt(SettingsObject input, int* external, char* section, char* name);
+    void GetItemInt(SettingsObject settings, int* external, char* section, char* name);
     
     /**
      * C-style wrapper for the getItem function for a double.
      *
-     * @param[in] input Settings object
+     * @param[in] settings Settings object
      * @param[out] external external double pointer to fill with requested data
      * @param[in] section name of the section where the requested item resides in the json file
      * @param[in] name name of the requested field in the json file
      */
-    void GetItemDbl(SettingsObject input, double* external, char* section, char* name);
+    void GetItemDbl(SettingsObject settings, double* external, char* section, char* name);
     
     /**
      * C-style wrapper for the getItem function for a vector<double>.
      *
-     * @param[in]  input Settings object
+     * @param[in]  settings Settings object
      * @param[out] external external array pointer to fill with requested data
      * @param[in]  size number of elements in external array
      * @param[in]  section name of the section where the requested item resides in the json file
      * @param[in]  name name of the requested field in the json file
      */
-    void GetItemDblArr(SettingsObject input, double external[], int* size, char* section, char* name);
+    void GetItemDblArr(SettingsObject settings, double external[], int* size, char* section, char* name);
 }
 
 #endif
--- cpp/src/settings.cpp
+++ cpp/src/settings.cpp
@@ -24,132 +24,132 @@
 
 using json = nlohmann::json;
 
-// Constructor for Input class
-Settings :: Settings(std::string input_file) {
+// Constructor for Settings class
+Settings :: Settings(std::string settings_file) {
     
-   // json input
-   json input;
-   readInputFile(input_file);
+   // json settings
+   json settings;
+   readSettingsFile(settings_file);
 }
 
-// Read the input file
-void Settings :: readInputFile(std::string input_file) {
+// Read the settings file
+void Settings :: readSettingsFile(std::string settings_file) {
     
     // read file and de-serialize
-    std::ifstream i(input_file);
-    i >> input;
+    std::ifstream i(settings_file);
+    i >> settings;
 }
 
-// Retrieve the requested integer from the input file
+// Retrieve the requested integer from the settings file
 void Settings :: getItem(int& external, std::string section, std::string name) {
-    external = input[section][name].get<int>();
+    external = settings[section][name].get<int>();
 }
 
-// Retrieve the requested double from the input file
+// Retrieve the requested double from the settings file
 void Settings :: getItem(double& external, std::string section, std::string name) {
-    external = input[section][name].get<double>();
+    external = settings[section][name].get<double>();
 }
 
-// Retrieve the requested vector<int> from the input file
+// Retrieve the requested vector<int> from the settings file
 void Settings :: getItem(std::vector<int>& external, std::string section, std::string name) {
-    external = input[section][name].get<std::vector<int> >();
+    external = settings[section][name].get<std::vector<int> >();
 }
 
-// Retrieve the requested vector<double> from the input file
+// Retrieve the requested vector<double> from the settings file
 void Settings :: getItem(std::vector<double>& external, std::string section, std::string name) {
-    external = input[section][name].get<std::vector<double> >();
+    external = settings[section][name].get<std::vector<double> >();
 }
 
-// Retrieve the requested vector<string> from the input file
+// Retrieve the requested vector<string> from the settings file
 void Settings :: getItem(std::vector<std::string>& external, std::string section, std::string name) {
-    external = input[section][name].get<std::vector<std::string> >();
+    external = settings[section][name].get<std::vector<std::string> >();
 }
 
 //////////////////////////////////////////////////////////////
 // C-style interface for compatibility with other languages //
 //////////////////////////////////////////////////////////////
 
-// C-style wrapper for the Input constructor
-SettingsObject GetInput(char* input_file) {
+// C-style wrapper for the Settings constructor
+SettingsObject GetSettings(char* settings_file) {
     
     // Convert from char* to std::string
-    std::string inputFile(input_file);
+    std::string settingsFile(settings_file);
 
     // Remove trailing spaces sent from Fortran
-    while(inputFile.size() && isspace(inputFile.back())) 
-        inputFile.pop_back();
+    while(settingsFile.size() && isspace(settingsFile.back())) 
+        settingsFile.pop_back();
     
     // Return Settings object
-    Input *input = new Input(inputFile);
+    Settings *settings = new Settings(settingsFile);
 
-    return (SettingsObject)input;
+    return (SettingsObject)settings;
 }
 
 // C-style wrapper for the getItem function for an integer
-void GetItemInt(SettingsObject input, int* external, char* section, char* name) {
+void GetItemInt(SettingsObject settings, int* external, char* section, char* name) {
     
     // Get Settings object
-    Settings* settings_obj = (Settings*)input;
+    Settings* settings_obj = (Settings*)settings;
         
     // Convert from char* to std::string
-    std::string inputSection(section);
-    std::string inputName(name);
+    std::string settingsSection(section);
+    std::string settingsName(name);
     
     // Remove trailing spaces sent from Fortran
-    while(inputSection.size() && isspace(inputSection.back())) 
-        inputSection.pop_back();
-    while(inputName.size() && isspace(inputName.back())) 
-        inputName.pop_back();
+    while(settingsSection.size() && isspace(settingsSection.back())) 
+        settingsSection.pop_back();
+    while(settingsName.size() && isspace(settingsName.back())) 
+        settingsName.pop_back();
     
     // Get item from Settings object
-    settings_obj->getItem(*external,inputSection,inputName);
+    settings_obj->getItem(*external,settingsSection,settingsName);
     
     return; 
 }
 
 // C-style wrapper for the getItem function for a double
-void GetItemDbl(SettingsObject input, double* external, char* section, char* name) {
+void GetItemDbl(SettingsObject settings, double* external, char* section, char* name) {
     
     // Get Settings object
-    Settings* settings_obj = (Settings*)input;
+    Settings* settings_obj = (Settings*)settings;
         
     // Convert from char* to std::string
-    std::string inputSection(section);
-    std::string inputName(name);
+    std::string settingsSection(section);
+    std::string settingsName(name);
     
     // Remove trailing spaces sent from Fortran
-    while(inputSection.size() && isspace(inputSection.back())) 
-        inputSection.pop_back();
-    while(inputName.size() && isspace(inputName.back())) 
-        inputName.pop_back();
+    while(settingsSection.size() && isspace(settingsSection.back())) 
+        settingsSection.pop_back();
+    while(settingsName.size() && isspace(settingsName.back())) 
+        settingsName.pop_back();
     
     // Get item from Settings object
-    settings_obj->getItem(*external,inputSection,inputName);
+    settings_obj->getItem(*external,settingsSection,settingsName);
     
     return; 
 }
 
 // C-style wrapper for the getItem function for a vector<double>
-void GetItemDblArr(SettingsObject input, double external[], int* size, char* section, char* name) {
+void GetItemDblArr(SettingsObject settings, double external[], int* size, char* section, char* name) {
     
     // Get Settings object
-    Settings* settings_obj = (Settings*)input;
+    Settings* settings_obj = (Settings*)settings;
         
     // Create a local vector
     std::vector<double> local(*size);
     
     // Convert from char* to std::string
-    std::string inputSection(section);
-    std::string inputName(name);
+    std::string settingsSection(section);
+    std::string settingsName(name);
     
     // Remove trailing spaces sent from Fortran
-    while(inputSection.size() && isspace(inputSection.back())) 
-        inputSection.pop_back();
-    while(inputName.size() && isspace(inputName.back())) 
-        inputName.pop_back();
+    while(settingsSection.size() && isspace(settingsSection.back())) 
+        settingsSection.pop_back();
+    while(settingsName.size() && isspace(settingsName.back())) 
+        settingsName.pop_back();
     
     // Get item from Settings object
-    settings_obj->getItem(local,inputSection,inputName);
+    settings_obj->getItem(local,settingsSection,settingsName);
     
     // Copy values from local vector to external array
     for (int i=0;i<*size;i++) {
